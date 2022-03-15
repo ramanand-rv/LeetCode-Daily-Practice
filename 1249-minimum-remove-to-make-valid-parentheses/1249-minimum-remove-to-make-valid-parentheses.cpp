ecd8e9fe100d35ee57108cab9f3402720a1b46ca
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2022-03-15 14:44:47 +0530

    Time: 23 ms (80.27%), Space: 11.6 MB (54.07%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack <int>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(')
+                st.push(i);
+            else if(s[i]==')'){
+                if(st.empty())
+                    s[i] = '#';
+                else
+                    st.pop();
+            }
+                
+        }
+        
+        while(!st.empty()){
+            s[st.top()] = '#';
+            st.pop();
+        }
+        
+        string res;
+        for(int i=0;i<s.size();i++){
+            if(s[i] != '#')
+                res += s[i];
+        }
+        return res;
+        
+    }
+};
