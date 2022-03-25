1872690d322d00ea63c8d264ae8f525391375ff6
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2022-03-26 05:29:07 +0530

    Time: 4 ms (78.71%), Space: 7.9 MB (78.35%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        sort(costs.begin(), costs.end(), [](const vector<int>&a, const vector<int>&b){
-            return (a[0] - a[1]) < (b[0] - b[1]);
-        });
+        vector<int> diff;
+        int mincost = 0;
         
-        int price = 0;
-        for(int i = 0; i < costs.size() / 2; i++){
-            price += costs[i][0];
+        for(int i = 0; i<costs.size(); ++i)
+        {
+            mincost += costs[i][0];
+            diff.push_back(costs[i][1] - costs[i][0]);
         }
-        for(int i = costs.size() / 2; i < costs.size(); i++){
-            price += costs[i][1];
+		
+        sort(diff.begin(),diff.end());
+        
+        for(int i=0; i<costs.size()/2; ++i)
+        {
+            mincost += diff[i];
         }
-        return price;
+        
+        return  mincost;
         
     }
 };
