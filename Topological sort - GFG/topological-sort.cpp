2109f8a7240e840c635245308edab53ca92463c9
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2023-08-16 13:39:39 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -10,30 +10,63 @@ class Solution
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
-	    stack<int>st;
+	   // stack<int>st;
 	    vector<bool>visited(V, false);
-	    for(int i=0;i<V;i++){
-	        if(!visited[i]){
-	            dfs(i, adj, visited, st);
-	        }
-	    }
+	   // for(int i=0;i<V;i++){
+	   //     if(!visited[i]){
+	   //        // dfs(i, adj, visited, st);
+	   //     }
+	   // }
 	    vector<int>res;
-	    while(!st.empty()){
-	        res.push_back(st.top());
-	        st.pop();
-	    }
-	    return res;
+	   // while(!st.empty()){
+	   //     res.push_back(st.top());
+	   //     st.pop();
+	   // }
+	   // return res;
+	   
+	   // Kahn's algo
+	   vector<int>inDeg(V, 0);
+	   for(int u=0;u<V;u++){
+	       for(int &v: adj[u]){
+	           inDeg[v]++;
+	       }
+	   }
+	   
+	   queue<int>q;
+	   for(int i=0;i<V;i++){
+	       if(inDeg[i]==0){
+	           q.push(i);
+	       }
+	   }
+	   
+	   while(!q.empty()){
+	       int u = q.front();
+	       q.pop();
+	       res.push_back(u);
+	       
+	       for(int &v: adj[u]){
+	           if(!visited[v]){
+	               if(--inDeg[v] ==0){
+	                   q.push(v);
+	               }
+	           }
+	       }
+	   }
+	   
+	   return res;
 	}
 	
-	void dfs(int u, vector<int>adj[], vector<bool>&visited, stack<int>&st){
-	    visited[u] = true;
-	    for(int &v: adj[u]){
-	        if(!visited[v]){
-	            dfs(v, adj, visited, st);
-	        }
-	    }
-	    st.push(u);
-	}
+
+	
+// 	void dfs(int u, vector<int>adj[], vector<bool>&visited, stack<int>&st){
+// 	    visited[u] = true;
+// 	    for(int &v: adj[u]){
+// 	        if(!visited[v]){
+// 	            dfs(v, adj, visited, st);
+// 	        }
+// 	    }
+// 	    st.push(u);
+// 	}
 };
 
 //{ Driver Code Starts.
