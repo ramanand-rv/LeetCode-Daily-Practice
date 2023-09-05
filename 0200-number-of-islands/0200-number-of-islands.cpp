3c7b97ee959eeeeccefcbe195f5b6cbb01f3c6ea
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2023-09-05 10:30:01 +0530

    Time: 30 ms (70.09%), Space: 18.4 MB (28.86%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -11,7 +11,8 @@ public:
         for(int row=0;row<N;row++){
             for(int col=0;col<M;col++){
                 if(!visited[row][col] && grid[row][col] == '1'){
-                    dfs(row, col, grid);
+                    // dfs(row, col, grid);
+                    bfs(row, col, grid);
                     islands++;
                 }
             }
@@ -19,29 +20,56 @@ public:
         return islands;   
     }
     
-    void dfs(int row, int col, vector<vector<char>>& grid){
-        visited[row][col] = true;
+//     Using DFS
+//     void dfs(int row, int col, vector<vector<char>>& grid){
+//         visited[row][col] = true;
        
-//         Returns the number of nodes in an island;
-//         for(int dr=-1;dr>=1;dr++){
-//             for(int dc=-1;dc>=1;dc++){
-//                 int nrow = row + dr;
-//                 int ncol = col + dc;
-//                 if(nrow<N && nrow>=0 && ncol<M && ncol>=0 
+// //         Returns the number of nodes in an island;
+// //         for(int dr=-1;dr>=1;dr++){
+// //             for(int dc=-1;dc>=1;dc++){
+// //                 int nrow = row + dr;
+// //                 int ncol = col + dc;
+// //                 if(nrow<N && nrow>=0 && ncol<M && ncol>=0 
+// //                && !visited[nrow][ncol] && grid[nrow][ncol] == '1'){
+// //                     dfs(nrow, ncol, grid);    
+// //             }
+// //         }
+        
+//         int delrow[] = {0,1,0,-1};
+//         int delcol[] = {1,0,-1,0};        
+//         for(int i=0;i<4;i++){
+//             int nrow = row + delrow[i];
+//             int ncol = col + delcol[i];
+
+//             if(nrow<N && nrow>=0 && ncol<M && ncol>=0 
 //                && !visited[nrow][ncol] && grid[nrow][ncol] == '1'){
-//                     dfs(nrow, ncol, grid);    
+//                 dfs(nrow, ncol, grid);    
 //             }
 //         }
-        
-        int delrow[] = {0,1,0,-1};
-        int delcol[] = {1,0,-1,0};        
-        for(int i=0;i<4;i++){
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
-
-            if(nrow<N && nrow>=0 && ncol<M && ncol>=0 
-               && !visited[nrow][ncol] && grid[nrow][ncol] == '1'){
-                dfs(nrow, ncol, grid);    
+//     }
+    
+    
+//     Using BFS
+    void bfs(int row, int col, vector<vector<char>>& grid){
+        queue<pair<int, int>>q;
+        q.push({row, col});
+        visited[row][col] = true;
+        while(!q.empty()){
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+            
+            int delrow[] = {1, 0, -1, 0};
+            int delcol[] = {0, 1, 0, -1};
+            
+            for(int i=0; i<4;i++){
+                int nr = r + delrow[i];
+                int nc = c + delcol[i];
+                
+                if(nr<N && nc<M && nr>=0 && nc>=0 && !visited[nr][nc] && grid[nr][nc]=='1'){
+                    q.push({nr, nc});
+                    visited[nr][nc] = true;
+                }
             }
         }
     }
