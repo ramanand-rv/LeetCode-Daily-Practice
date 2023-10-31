cf3de37d9c49aa644275f3bdb33e4c3367cf0006
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2023-10-31 12:55:36 +0530

    Time: 467 ms (43.07%), Space: 209.9 MB (10.93%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
     int mod = 1e9 + 7;
+    int N, M;
     int numberOfPaths(vector<vector<int>>& grid, int k) {
-        vector<vector<vector<int>>>dp(grid.size(),
-                                      vector<vector<int>>(
-                                          grid[0].size(),
-                                          vector<int>(51, -1)));
+        N=grid.size(); M=grid[0].size();
+        vector<vector<vector<int>>>dp(N, vector<vector<int>>(
+                                      M, vector<int>(51, -1)));
         return solve(grid, k, 0, 0, 0, dp);
     }
     
     int solve(vector<vector<int>>& grid, int k, int i, int j, int sum, 
               vector<vector<vector<int>>>&dp){
-
-        if(i<0||j<0||i>=grid.size()||j>=grid[0].size())
+        if(i<0||j<0||i>=N||j>=M)
             return 0;
-        if( i==size(grid)-1 && j==size(grid[0])-1 ){
+        if( i == N-1 && j == M-1 ){
             sum += grid[i][j];
             return sum%k==0?1:0;
         }
         if(dp[i][j][sum%k] != -1) return dp[i][j][sum%k];
 
-
-
-    //     Move Right
+        //  Move Right
         long long int rightSum = solve(grid, k, i, j+1, sum+grid[i][j], dp);
 
-    //     Move Down
+        //  Move Down
         long long int downSum = solve(grid, k, i+1, j, sum+grid[i][j], dp);
 
         return dp[i][j][sum%k] = ((rightSum % mod) + (downSum % mod)) % mod;
