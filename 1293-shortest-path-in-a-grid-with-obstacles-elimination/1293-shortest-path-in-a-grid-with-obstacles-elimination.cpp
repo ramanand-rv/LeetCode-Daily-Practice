395cb60aa6a92cfda70eba4e9430476f63250bd0
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2023-11-06 17:40:50 +0530

    Time: 180 ms (31.72%), Space: 15.8 MB (71.62%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,53 +1,68 @@
 class Solution {
 public:
     
-//    Clears 50% TCs (using BFS) 
-  int shortestPath(vector<vector<int>>& grid, int k) {
-        int m = grid.size();
-        int n = grid[0].size();
-        if(m==1 && n ==1) return 0;
-        int steps = 0;
-        bool visited[41][41][1681];
-        memset(visited, false, sizeof(visited));
-        vector<vector<int>>directions = {{1, 0}, {0, 1}, 
-                                         {0, -1}, {-1, 0}};
-        queue<vector<int>>q;
-        auto isSafe = [&] (int r, int c){
-            return r>=0 && c>=0 && r<m && c<n;
-        };
+//    Passes 100% TCs (using BFS) 
+//     1. In Queue and Visited store {m, n, k}
+//     2. Start BFS Traversal, count number of steps;
+//          if x==m-1 && y==n-1 return steps
+    
+//     3. If grid[m][n]==0 (no wall) && [m, n, k] is not visited:
+//          push {m, n, k} in q;
+//          and mark [m, n, k] as visited 
+    
+//     4. If grid[m][n]==1 (wall) && [m, n, k-1] is not visited:
+//          push {m, n, k-1} in q;
+//          and mark [m, n, k-1] as visited
+    
+//     5. Return -1 on finishing the traversal
+    
+    
+//   int shortestPath(vector<vector<int>>& grid, int k) {
+//         int m = grid.size();
+//         int n = grid[0].size();
+//         if(m==1 && n ==1) return 0;
+//         int steps = 0;
+//         bool visited[41][41][1681];
+//         memset(visited, false, sizeof(visited));
+//         vector<vector<int>>directions = {{1, 0}, {0, 1}, 
+//                                          {0, -1}, {-1, 0}};
+//         queue<vector<int>>q;
+//         auto isSafe = [&] (int r, int c){
+//             return r>=0 && c>=0 && r<m && c<n;
+//         };
 
-        q.push({0, 0, k});
-        visited[0][0][k] = true;
+//         q.push({0, 0, k});
+//         visited[0][0][k] = true;
 
-        while(!q.empty()){
-            int sz = q.size();
-            while(sz--){
-                auto cur = q.front();
-                q.pop();
+//         while(!q.empty()){
+//             int sz = q.size();
+//             while(sz--){
+//                 auto cur = q.front();
+//                 q.pop();
                 
-                if(cur[0]==m-1 && cur[1]==n-1) return steps;
+//                 if(cur[0]==m-1 && cur[1]==n-1) return steps;
                 
-                for(auto &dir: directions){
-                    int x_ = cur[0] + dir[0];
-                    int y_ = cur[1] + dir[1];
-                    int obs = cur[2];
+//                 for(auto &dir: directions){
+//                     int x_ = cur[0] + dir[0];
+//                     int y_ = cur[1] + dir[1];
+//                     int obs = cur[2];
 
-                    if(isSafe(x_, y_)){
-                        if(grid[x_][y_]==0 && !visited[x_][y_][obs]){
-                            q.push({x_, y_, obs});
-                            visited[x_][y_][obs] = true;
-                        }
-                        else if(grid[x_][y_]==1 && obs>0 && !visited[x_][y_][obs-1]){
-                            q.push({x_, y_, obs-1});
-                            visited[x_][y_][obs-1] = true;
-                        }
-                    }
-                }
-            }
-            steps++;
-        }
-        return -1;;
-    }
+//                     if(isSafe(x_, y_)){
+//                         if(grid[x_][y_]==0 && !visited[x_][y_][obs]){
+//                             q.push({x_, y_, obs});
+//                             visited[x_][y_][obs] = true;
+//                         }
+//                         else if(grid[x_][y_]==1 && obs>0 && !visited[x_][y_][obs-1]){
+//                             q.push({x_, y_, obs-1});
+//                             visited[x_][y_][obs-1] = true;
+//                         }
+//                     }
+//                 }
+//             }
+//             steps++;
+//         }
+//         return -1;;
+//     }
 
     
     
@@ -64,47 +79,46 @@ public:
 //     3. if number of walls is <= K return the number of steps
 //     4. Else return -1
     
-//     int shortestPath(vector<vector<int>>& grid, int k) {
-//         int m = grid.size();
-//         int n = grid[0].size();
-//         int steps = 0;
+    int shortestPath(vector<vector<int>>& grid, int k) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int steps = 0;
         
-//         if(m==1 && n ==1) return 0;
+        if(m==1 && n ==1) return 0;
         
-//         vector<vector<int>>dp(m, vector<int>(n, INT_MAX));
-//         vector<vector<bool>>visited(m, vector<bool>(n, false));
-//         vector<vector<int>>directions = {{1, 0}, {0, 1}, 
-//                                          {0, -1}, {-1, 0}};
-//         queue<pair<int, pair<int, int>>>q;
-//         auto isSafe = [&] (int r, int c){
-//             return r>=0 && c>=0 && r<m && c<n && !visited[r][c];
-//         };
-        
-//         q.push({grid[0][0], {0, 0}});
-                
-//         while(!q.empty()){
-//             int qSize = q.size();
-//             steps++;
-//             while(qSize--){
-//                 int w = q.front().first;
-//                 auto coord = q.front().second;
-//                 int x = coord.first;
-//                 int y = coord.second;
-//                 q.pop();
-//                 for(auto &dir: directions){
-//                     int x_ = x + dir[0];
-//                     int y_ = y + dir[1];
-                    
-//                     if(isSafe(x_, y_)){
-//                         if(dp[x_][y_] > w + grid[x_][y_]){
-//                             dp[x_][y_] = w + grid[x_][y_];
-//                             q.push({w+grid[x_][y_], {x_, y_}});
-//                             visited[x_][y_] = true;
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//         return dp[m-1][n-1] <= k ? steps-1 : -1;
-//     }
+        vector<vector<int>>dp(m, vector<int>(n, -1));
+        bool visited[41][41][1681];
+        memset(visited, false, sizeof(visited));
+        vector<vector<int>>directions = {{1, 0}, {0, 1}, 
+                                         {0, -1}, {-1, 0}};
+        priority_queue<vector<int>, 
+            vector<vector<int>>, 
+            greater<vector<int>> >q;
+        auto isSafe = [&] (int r, int c){
+            return r>=0 && c>=0 && r<m && c<n;
+        };
+//         {steps, k, x, y}
+        q.push({0, k, 0, 0});
+        while(!q.empty()){
+            int steps = q.top()[0];
+            int canBreak = q.top()[1];
+            int x = q.top()[2];
+            int y = q.top()[3];
+            q.pop();
+            if(x==m-1 && y==n-1) return steps;
+            for(auto &dir: directions){
+                int x_ = x + dir[0];
+                int y_ = y + dir[1];
+
+                if(isSafe(x_, y_)){
+                    int rem = canBreak - (grid[x_][y_]==1);
+                    if(rem >=0 && rem > dp[x_][y_] ){
+                        q.push({steps+1, rem, x_, y_});
+                        dp[x_][y_] = rem;
+                    }
+                    }
+                }
+            }
+        return -1;
+        }
 };
