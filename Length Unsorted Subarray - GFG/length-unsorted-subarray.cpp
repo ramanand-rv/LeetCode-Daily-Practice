fd761def95aea3d2bbe631b4b42e563f54e3927b
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2023-12-28 17:45:13 +0530

    Added solution - LeetHub

---
+++ Length Unsorted Subarray - GFG/length-unsorted-subarray.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	vector<int> printUnsorted(int arr[], int n) {
+	    if(n==0) return {0, 0};
+	    int l=0, r=0;
+	    int mx = arr[0];
+	    
+	    for(int i=0;i<n;i++){
+	        if(arr[i] < mx) l = i;
+            mx = max(mx, arr[i]);
+	    }
+	    
+	    int mn = arr[n-1];
+	    for(int i=n-1;i>=0;i--){
+	        if(arr[i] > mn) r=i;
+	        mn = min(mn, arr[i]);
+	    }
+	    return {r, l};
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.printUnsorted(arr, n);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
