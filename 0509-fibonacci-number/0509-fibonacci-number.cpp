2ceb71b4d7d0d76e7ddf694316ef4941774176ae
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2025-10-05 13:45:40 +0530

    Time: 0 ms (100%), Space: 7.8 MB (36.04%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,48 @@
 class Solution {
 public:
+/*
+// Memoization
+    int fibo(int n, int memo[]){
+        if(n <= 1) return n;
 
-    int recur(int n){
-        if(n == 0) return 0;
-        if(n == 1) return 1;
+        if(memo[n] == -1) memo[n] = fibo(n-1, memo) + fibo(n-2, memo);
 
-        return recur(n-1) + recur(n-2);
+        return memo[n];
     }
 
     int fib(int n) {
-        return recur(n);
+        int memo[100];
+        memset(memo, -1, sizeof(memo));
+
+        return fibo(n, memo);
+    }
+*/
+
+/*
+// Tabulation
+    int fib(int n){
+        if(n <= 1) return n;
+
+        vector<int>dp(n+1);
+        dp[0] = 0; 
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+*/
+
+// Memory Optimization
+    int fib(int n){
+        if( n <= 1) return n;
+        int curr = 1, prev = 0;
+
+        for(int i=2; i<=n; i++){
+            curr = curr + prev;
+            prev = curr - prev;
+        }
+        return curr;
     }
 };
