a83cfdf518682cc5426be0633acffdc13b272a39
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2025-10-06 13:49:01 +0530

    Time: 0 ms (100%), Space: 10.1 MB (80.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
+
+// Memoization
     int rob(int i, vector<int>&money, int memo[]){
-        if(i >= money.size()) return 0;
-        if(memo[i] == -1) memo[i] = money[i] + max(rob(i+2, money, memo), rob(i+3, money, memo));
+        if(i < 0) return 0;
+        if(memo[i] == -1) memo[i] = max(money[i] + rob(i-2, money, memo), rob(i-1, money, memo));
         return memo[i];
     }
 
     int rob(vector<int>& nums) {
         int memo[500];
         memset(memo, -1, sizeof(memo));
-        return max(rob(0, nums, memo), rob(1, nums, memo));
+        return rob(nums.size()-1, nums, memo);
     }
+
+
 };
