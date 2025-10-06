fcc007684f5c10b54cc5b3ffc24a1cd6791ba414
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2025-10-06 15:03:45 +0530

    Time: 0 ms (100%), Space: 10.1 MB (90.36%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -14,7 +14,8 @@ public:
         return rob(nums.size()-1, nums, memo);
     }
 */
-
+/*
+// Tabulation
     int rob(vector<int>&nums){
         int n = nums.size();
         if(n == 1) return nums[0];
@@ -26,5 +27,17 @@ public:
         }
         return dp[n-1];
     }
+*/
+    int rob(vector<int>&nums){
+        int n = nums.size();
+        if(n==1) return nums[0];
 
+        int prev2 = nums[0], prev1 = max(nums[0], nums[1]);
+        for(int i=2; i<n; i++){
+            int curr = max(nums[i] + prev2, prev1);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
 };
