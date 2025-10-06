ce4f84010b8bfc800c47db726cdc0f321afb0cb9
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2025-10-06 05:43:08 +0530

    Time: 0 ms (100%), Space: 17.6 MB (74.67%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+/*
+// Memoization
     int minCost(int i, vector<int>&cost, int memo[]){
         if(i >= cost.size()) return 0;
 
@@ -14,4 +16,19 @@ public:
         int n = cost.size();
         return min(minCost(0, cost, memo), minCost(1, cost, memo));
     }
+*/
+
+    int minCostClimbingStairs(vector<int>& cost){
+        int n = cost.size();
+        vector<int>dp(n+10);
+
+        dp[0] = cost[0]; dp[1] = cost[1]; 
+
+        for(int i=2; i<n; i++){
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+        }
+
+        return min(dp[n-1], dp[n-2]);
+    }
+
 };
