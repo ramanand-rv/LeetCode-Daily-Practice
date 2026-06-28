99926fe238e89e8a9de2aa1f19df40dca8ba0ebb
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2026-06-28 23:16:04 +0530

    Time: 7 ms (100%), Space: 66.2 MB (100%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.ts
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.ts
@@ -2,6 +2,7 @@ function maximumElementAfterDecrementingAndRearranging(arr: number[]): number {
     const n = arr.length;
 
     arr.sort((a, b) => a - b);
+/*
 
     arr[0] = 1;
 
@@ -11,4 +12,13 @@ function maximumElementAfterDecrementingAndRearranging(arr: number[]): number {
         }
     }
     return arr.at(-1)
+*/
+    let target = 1;
+
+    for(const v of arr){
+        if(v >= target){
+            target++;
+        }
+    }
+    return target-1;
 };
