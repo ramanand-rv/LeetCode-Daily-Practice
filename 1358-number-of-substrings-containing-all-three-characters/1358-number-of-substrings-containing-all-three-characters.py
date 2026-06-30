1d41de894097fef27833030e440633f61427608d
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2026-07-01 00:16:07 +0530

    Time: 96 ms (66.77%), Space: 19.6 MB (15.25%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -1,28 +1,41 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        n = len(s)
-        left = 0
-        cnt = 0
-        a = b = c = 0
 
-        for right in range(n):
-            if s[right] == 'a':
-                a += 1
-            elif s[right] == 'b':
-                b += 1
-            else:
-                c += 1
+        # n = len(s)
+        # left = 0
+        # cnt = 0
+        # a = b = c = 0
 
-            while a > 0 and b > 0 and c > 0:
-                cnt += (n - right)
+        # for right in range(n):
+        #     if s[right] == "a":
+        #         a += 1
+        #     elif s[right] == "b":
+        #         b += 1
+        #     else:
+        #         c += 1
 
-                if s[left] == 'a':
-                    a -= 1
-                elif s[left] == 'b':
-                    b -= 1
-                else:
-                    c -= 1
-                
-                left += 1
+        #     while a > 0 and b > 0 and c > 0:
+        #         cnt += n - right
 
-        return cnt
+        #         if s[left] == "a":
+        #             a -= 1
+        #         elif s[left] == "b":
+        #             b -= 1
+        #         else:
+        #             c -= 1
+
+        #         left += 1
+
+        # return cnt
+
+        seen = {'a': -1, 'b': -1, 'c': -1}
+        count = 0
+        
+        for i, ch in enumerate(s):
+            seen[ch] = i
+            
+            if seen['a'] != -1 and seen['b'] != -1 and seen['c'] != -1:
+                minPos = min(seen.values())
+                count += (minPos + 1)
+        
+        return count
