5b22759530bb8eb2685e098f1a85d8b3804af012
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2026-07-01 00:22:32 +0530

    Time: 1 ms (83.33%), Space: 2.3 MB (33.33%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.rs
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.rs
@@ -1,5 +1,8 @@
+use std::cmp::min;
+
 impl Solution {
     pub fn number_of_substrings(s: String) -> i32 {
+/*
         let n = s.len();
         let mut left = 0;
         let mut cnt = 0;
@@ -29,5 +32,20 @@ impl Solution {
         }
 
         cnt
+*/
+        let s = s.as_bytes();
+        let mut seen = [-1, -1, -1];
+        let mut count = 0;
+        
+        for (i, &ch) in s.iter().enumerate() {
+            seen[(ch - b'a') as usize] = i as i32;
+            
+            if seen.iter().all(|&x| x != -1) {
+                let min_pos = min(seen[0], min(seen[1], seen[2]));
+                count += (min_pos + 1);
+            }
+        }
+        
+        count
     }
 }
