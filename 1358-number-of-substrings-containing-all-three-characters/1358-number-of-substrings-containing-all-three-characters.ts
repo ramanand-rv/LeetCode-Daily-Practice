38448a53eb36eaae09f645050cc9bab7bc3d55c7
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2026-07-01 00:17:52 +0530

    Time: 11 ms (77.42%), Space: 57 MB (61.29%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.ts
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.ts
@@ -1,24 +1,40 @@
 function numberOfSubstrings(s: string): number {
-    const n = s.length;
-    let left = 0;
-    let cnt = 0;
-    let a = 0, b = 0, c = 0;
+    /*
+        const n = s.length;
+        let left = 0;
+        let cnt = 0;
+        let a = 0, b = 0, c = 0;
+    
+        for (let right = 0; right < n; right++) {
+            if (s[right] === 'a') a++;
+            else if (s[right] === 'b') b++;
+            else c++;
+    
+            while (a > 0 && b > 0 && c > 0) {
+                cnt += (n - right);
+    
+                if (s[left] === 'a') a--;
+                else if (s[left] === 'b') b--;
+                else c--;
+    
+                left++;
+            }
+        }
+    
+        return cnt;
+    */
+    const seen = { a: -1, b: -1, c: -1 };
+    let count = 0;
 
-    for (let right = 0; right < n; right++) {
-        if (s[right] === 'a') a++;
-        else if (s[right] === 'b') b++;
-        else c++;
+    for (let i = 0; i < s.length; i++) {
+        seen[s[i]] = i;
 
-        while (a > 0 && b > 0 && c > 0) {
-            cnt += (n - right);
-
-            if (s[left] === 'a') a--;
-            else if (s[left] === 'b') b--;
-            else c--;
-
-            left++;
+        if (seen.a !== -1 && seen.b !== -1 && seen.c !== -1) {
+            const minPos = Math.min(seen.a, seen.b, seen.c);
+            count += (minPos + 1);
         }
     }
 
-    return cnt;
+    return count;
+
 };
