e45e6c1d724847d7dc167b624de6921259680d43
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2026-07-01 00:10:56 +0530

    Time: 4 ms (79.36%), Space: 11.2 MB (33.33%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int n = s.size();
+/*        int n = s.size();
         int left = 0;
         int cnt = 0;
         int a = 0, b = 0, c = 0;
@@ -29,5 +29,20 @@ public:
         }
 
         return cnt;
+*/
+        int seen[3] = {-1, -1, -1};
+        int count = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            seen[s[i] - 'a'] = i;
+            
+            if (seen[0] != -1 && seen[1] != -1 && seen[2] != -1) {
+                
+                int minPos = std::min({seen[0], seen[1], seen[2]}); 
+                count += (minPos + 1);
+            }
+        }
+        
+        return count;
     }
 };
