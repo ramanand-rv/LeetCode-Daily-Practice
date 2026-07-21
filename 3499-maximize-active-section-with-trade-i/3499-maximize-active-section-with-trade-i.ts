f39b227ea0e2874aae4afbadba6512d2834ebffe
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2026-07-21 23:45:51 +0530

    Time: 63 ms (100%), Space: 71.8 MB (93.75%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.ts
@@ -0,0 +1,30 @@
+function maxActiveSectionsAfterTrade(s: string): number {
+    const n = s.length;
+    let prevZero = 0;
+    let currZero = 0;
+    let totalOnes = 0;
+    let best = 0;
+    let i = 0;
+
+    while (i < n) {
+        if (s[i] === '0') {
+            prevZero++;
+            i++;
+        } else {
+            while (i < n && s[i] === '1') {
+                totalOnes++;
+                i++;
+            }
+            while (i < n && s[i] === '0') {
+                currZero++;
+                i++;
+            }
+            if (prevZero && currZero) {
+                best = Math.max(best, prevZero + currZero);
+            }
+            prevZero = currZero;
+            currZero = 0;
+        }
+    }
+    return totalOnes + best;
+}
