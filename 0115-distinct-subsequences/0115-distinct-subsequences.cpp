1c05c2a4cfa99a47f716caec74638408da3b5179
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2026-09-06 23:13:49 +0530

    Time: 27 ms (71.17%), Space: 27 MB (64.23%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    int solve(int i, int j, string& s, string& t, vector<vector<int>>& memo) {
-
-        if (j == t.size())
-            return 1;
-        if (i == s.size())
-            return 0;
-
-        if (memo[i][j] != -1)
-            return memo[i][j];
-
-        int notTake = solve(i + 1, j, s, t, memo);
-        int take = 0;
-        if (s[i] == t[j]) {
-            take = solve(i + 1, j + 1, s, t, memo);
-        }
-        return memo[i][j] = take + notTake;
-    }
-
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> memo(n, vector<int>(m, -1));
-        return solve(0, 0, s, t, memo);
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        int MOD = (int)1e9 + 7; // to avoid int overflow
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][m] = 1;
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                dp[i][j] = dp[i + 1][j];
+
+                if (s[i] == t[j]) {
+                    dp[i][j] = (dp[i][j] + dp[i + 1][j + 1]) % MOD;
+                }
+            }
+        }
+
+        return dp[0][0];
     }
 };
