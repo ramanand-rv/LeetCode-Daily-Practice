df8d764325c945420fc830c5d12d285a7d6b065e
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2026-09-14 23:47:17 +0530

    Time: 0 ms (100%), Space: 2.1 MB (25%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.rs
+++ 0836-rectangle-overlap/0836-rectangle-overlap.rs
@@ -1,9 +1,6 @@
 impl Solution {
     pub fn is_rectangle_overlap(rec1: Vec<i32>, rec2: Vec<i32>) -> bool {
-        let left   = rec1[0].max(rec2[0]);
-        let right  = rec1[2].min(rec2[2]);
-        let bottom = rec1[1].max(rec2[1]);
-        let top    = rec1[3].min(rec2[3]);
-        left < right && bottom < top
+        rec1[2] > rec2[0] && rec1[3] > rec2[1] &&
+        rec1[0] < rec2[2] && rec1[1] < rec2[3]
     }
 }
