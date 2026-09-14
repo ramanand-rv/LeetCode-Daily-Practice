60d09be8f1a194e37b743c5ec6d2d7d39198107b
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2026-09-14 23:45:57 +0530

    Time: 0 ms (100%), Space: 55 MB (51.61%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.ts
+++ 0836-rectangle-overlap/0836-rectangle-overlap.ts
@@ -1,7 +1,4 @@
 function isRectangleOverlap(rec1: number[], rec2: number[]): boolean {
-    const left = Math.max(rec1[0], rec2[0]);
-    const right = Math.min(rec1[2], rec2[2]);
-    const bottom = Math.max(rec1[1], rec2[1]);
-    const top = Math.min(rec1[3], rec2[3]);
-    return left < right && bottom < top;
+    return rec1[2] > rec2[0] && rec1[3] > rec2[1] &&
+        rec1[0] < rec2[2] && rec1[1] < rec2[3];
 }
